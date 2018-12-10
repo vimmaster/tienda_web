9c2281c469ef7a23539dad172f92c4384f09109e
Ricard Sanchez <ricard.sanchezBe@e-campus.uab.cat>
2018-12-10 13:01:05 +0100

    Hecho panel de usuario con AJAX

--- view/list-categories.php
+++ view/list-categories.php
@@ -4,6 +4,9 @@
     <title>portatilbarato.com</title>
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="/view/css/estilo.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css"
+          integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU"
+          crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="/view/scripts.js"></script>
 </head>
@@ -12,6 +15,11 @@
         <a href="/"><h1>PortatilBarato</h1></a>
         <a href="../controller/login.php"><h2 class="right">LOG-IN</h2></a>
         <a href="../controller/register.php"><h2 class="right">REGISTER</h2></a>
+        <button id="button-profile"><i class="fas fa-user-circle"> </i></button>
+        <div id="panel-profile">
+            <div> Login</div>
+            <div> Register</div>
+        </div>
     </header>
     <section id="1">
         <div class="grid-container">
