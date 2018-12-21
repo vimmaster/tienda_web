56e0249b5cf4a162cc2319189d5c27f0fabc16e9
Ricard Sanchez <ricard.sanchezBe@e-campus.uab.cat>
2018-12-21 17:37:08 +0100

    Añadidas opciones al menu de usuario

--- view/header.php
+++ view/header.php
@@ -3,7 +3,10 @@
 <div id="container-profile">
     <button id="button-profile"><i class="fas fa-user-circle"> </i></button>
     <div id="panel-profile" class="grid-item">
-        <a href="../controller/login.php"><h2>LOG-IN</h2></a>
-        <a href="../controller/register.php"><h2>REGISTER</h2></a>
+        <a href="../controller/login.php"><h>LOG-IN</h></a>
+        <a href="../controller/register.php"><h>REGISTER</h></a>
+        <a href="#"><h>Mi cuenta</h></a>
+        <a href="#"><h>Mis pedidos</h></a>
+        <a href="#"><h>Cerrar sessión</h></a>
     </div>
 </div>
